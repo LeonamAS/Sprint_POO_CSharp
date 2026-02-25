0eacbea9dab7ca792cbc967a3503129523e06117
LeonamAS <leonam.sales@ba.estudante.senai.br>
2026-02-25 11:38:21 -0300

    Primeira tentativa

--- Sprint_POO-CSharp/Sprint_POO-CSharp/Modelos/Professor.cs
+++ Sprint_POO-CSharp/Sprint_POO-CSharp/Modelos/Professor.cs
@@ -1,7 +1,18 @@
 ﻿namespace Sprint_POO_CSharp.Modelos;
 
-internal class Professor
+internal class Professor : Pessoa
 {
-    public int Salario { get; set; }
-    private List<string> turmas = new List<string>();
+    private double Salario { get; set; }
+    private List<string> Turmas { get; set; } = new List<string>();
+
+    public Professor(string nome, int cpf, DateTime dataNascimento, double salario)
+        : base(nome, cpf, dataNascimento)
+    {
+        Salario = salario;
+    }
+
+    public override void ExibirDados()
+    {
+        //Console.WriteLine($"[Professor] Nome: {Nome} | Salário: R${Salario:F2}");
+    }
 }
--- Sprint_POO-CSharp/Sprint_POO-CSharp/Program.cs
+++ Sprint_POO-CSharp/Sprint_POO-CSharp/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿using Sprint_POO_CSharp.Modelos;
+
+//Easy English
+string mensagemDeBoasVindas = "Bem-vindo a Easy English";
+
+//Dictionary<int, Menu> opcoes = new();
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+███████╗░█████╗░░██████╗██╗░░░██╗  ███████╗███╗░░██╗░██████╗░██╗░░░░░██╗░██████╗██╗░░██╗
+██╔════╝██╔══██╗██╔════╝╚██╗░██╔╝  ██╔════╝████╗░██║██╔════╝░██║░░░░░██║██╔════╝██║░░██║
+█████╗░░███████║╚█████╗░░╚████╔╝░  █████╗░░██╔██╗██║██║░░██╗░██║░░░░░██║╚█████╗░███████║
+██╔══╝░░██╔══██║░╚═══██╗░░╚██╔╝░░  ██╔══╝░░██║╚████║██║░░╚██╗██║░░░░░██║░╚═══██╗██╔══██║
+███████╗██║░░██║██████╔╝░░░██║░░░  ███████╗██║░╚███║╚██████╔╝███████╗██║██████╔╝██║░░██║
+╚══════╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ╚══════╝╚═╝░░╚══╝░╚═════╝░╚══════╝╚═╝╚═════╝░╚═╝░░╚═╝
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para cadastrar um novo aluno");
+    Console.WriteLine("Digite 2 para cadastrar um novo professor");
+    Console.WriteLine("Digite 3 para mostrar lista de pessoas cadastradas");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    //if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+    //{
+    //    Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+    //    menuASerExibido.Executar();
+    //    if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Opção inválida");
+    //}
+}
+
+//var pessoas = new List<Pessoa>();
+
+//var aluno1 = new Aluno("João", 123, new DateTime(2005, 5, 10), 01);
+//aluno1.Notas.AddRange(new[] { 8.5, 7.0, 9.0 });
+
+//var prof1 = new Professor("Dr. Smith", 456, new DateTime(1980, 1, 1), 5000.00);
+
+//pessoas.Add(aluno1);
+//pessoas.Add(prof1);
+
+//Console.WriteLine("--- Listagem Acadêmica ---");
+//foreach (var pessoa in pessoas)
+//{
+//    pessoa.ExibirDados();
+//}
+
+//double mediaGeralAlunos = pessoas.OfType<Aluno>().Average(a => a.CalcularMedia());
+//Console.WriteLine($"\nMédia Geral dos Alunos: {mediaGeralAlunos:F2}");
+
+ExibirOpcoesDoMenu();
